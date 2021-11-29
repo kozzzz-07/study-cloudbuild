d3800a15ecab399874c61e4f28b08ec1b6fc6409
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-11-29 14:13:29 +0900

    init client

---
+++ client/sample-app/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Tutorial />
+    <button @click="fetchData()">fetch</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async fetchData() {
+      try {
+        const resp = await this.$axios.$get(
+          "http://localhost:8080/api/test?test=aaaa"
+        );
+        console.log(resp);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+};
+</script>
